0de54cc411d3eb980c1f388b86e07a174586a13f
jcrios <cesarzzupo@gmail.com>
2024-12-31 18:04:47 -0600

    add whist movement tracking

--- app/pose_estimation_system.py
+++ app/pose_estimation_system.py
@@ -1,13 +1,15 @@
 import cv2
 import torch
 from ultralytics import YOLO
+from math import dist
+import numpy as np
 
 # Load YOLOv8 pose estimation model
-model = YOLO('yolov8s-pose.pt')  # Ensure you have downloaded the model
+model = YOLO('yolov8l-pose.pt')  # Ensure you have downloaded the model
 
 # Input and output paths
-input_video_path = './/..//res//videos//example.mp4'  # Replace with your video path
-output_video_path = './/..//res//videos//example_result.mp4'  # Output path
+input_video_path = './/..//res//videos//source//example.mp4'  # Replace with your video path
+output_video_path = './/..//res//videos//results//example_result.mp4'  # Output path
 
 # Open the video file
 cap = cv2.VideoCapture(input_video_path)
@@ -28,6 +30,13 @@ out = cv2.VideoWriter(output_video_path, fourcc, fps, (frame_width, frame_height
 
 print("Processing video...")
 frame_count=0
+movements = 0
+whist = False
+left_whist = (0,0)
+right_whist_last = (0,0)
+left_whist_last = (0,0)
+right_whist_last = (0,0)
+start_frame =  146
 while True:
     ret, frame = cap.read()
 
@@ -36,11 +45,15 @@ while True:
 
     frame_count += 1  
     # Run pose estimation
-    results = model(frame)
+    results = model(frame, conf=0.7)
 
     # Visualize the results on the frame
     annotated_frame = results[0].plot()
     
+    #=========================================================
+    ##Need to define the thresholds
+    #=========================================================
+    #left_whist_conf_tresh = [keypoints_conf[i] for i in [9, 10, 15, 16]]
      # Add frame counter to the top-right corner
     '''
     text = f"Frame: {frame_count}"
@@ -53,8 +66,69 @@ while True:
     text_y = 30
     cv2.putText(annotated_frame, text, (text_x, text_y), font, font_scale, color, thickness)
     '''
-    movements = 0
-    if frame_count>145:
+    #left_whist_last, right_whist_last, left_ankle_last, right_ankle_last=torch.ones(4)
+    if frame_count >= start_frame and whist == False:
+        ## Initial Keypoints
+        try:
+            keypoints_conf = results[0].keypoints.conf[0]
+            keypoints_yolo = results[0].keypoints.xy[0]
+            left_whist_last = keypoints_yolo[9] if keypoints_yolo[9][0]>0 and keypoints_yolo[9][1]>0 and keypoints_conf[9]>0.7  else left_whist_last
+            right_whist_last = keypoints_yolo[10] if keypoints_yolo[10][0]>0 and keypoints_yolo[10][1]>0 and keypoints_conf[10]>0.7 else right_whist_last
+            #, right_whist_last, left_ankle_last, right_ankle_last =[keypoints_yolo[i] for i in [9, 10, 15, 16]]
+        except: 
+            None
+        if all(x > 0 for x in left_whist_last) and all(x > 0 for x in right_whist_last):
+            whist = True
+    elif frame_count > start_frame and whist == True:
+        #define the var to store the last position of the keypoints of interest
+    
+        #c_w= results[0].keypoints.conf[0][9]> 0.7
+        #print(1 if c_w.numpy()==True else 0)
+        #extract c
+        try:
+            keypoints_conf = results[0].keypoints.conf[0]
+            keypoints_yolo = results[0].keypoints.xy[0]
+            left_whist = keypoints_yolo[9] if keypoints_yolo[9][0]>0 and keypoints_yolo[9][1]>0 and keypoints_conf[9]>0.7  else left_whist
+            right_whist = keypoints_yolo[10] if keypoints_yolo[10][0]>0 and keypoints_yolo[10][1]>0 and keypoints_conf[10]>0.7 else right_whist
+            #left_whist_conf, right_whist_conf, left_ankle_conf, right_ankle_conf = [keypoints_conf[i] for i in [9, 10, 15, 16]]
+            #left_whist, right_whist, left_ankle, right_ankle = [keypoints_yolo[i] for i in [9, 10, 15, 16]]
+        except:
+            None
+        if all(x > 0 for x in left_whist) and all(x > 0 for x in right_whist):
+            distance_whist_last =  dist(left_whist_last,right_whist_last ) if dist(left_whist_last,right_whist_last )>0 else 1
+            distance_whist =  dist(left_whist, right_whist)
+            distance_whist_change =  np.round(np.abs((distance_whist - distance_whist_last)/distance_whist_last),2)
+            if distance_whist_change>0.8:
+                movements+=1
+            if keypoints_yolo[9][0]>0 and keypoints_yolo[9][1]>0:
+                left_whist_last =keypoints_yolo[9]
+            if keypoints_yolo[10][0]>0 and keypoints_yolo[10][1]:
+                right_whist_last =keypoints_yolo[10]
+        
+        #left_whist_last, right_whist_last, left_ankle_last, right_ankle_last =[keypoints_yolo[i] for i in [9, 10, 15, 16]]
+
+        ##Distance annotations
+        text_whist = f"Whist movement: "#{distance_whist_change}"
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 1
+        color = (255, 250, 250)  # Dark blue in BGR
+        thickness = 2
+        text_size = cv2.getTextSize(text_whist, font, font_scale, thickness)[0]
+        text_x = frame_width - text_size[0] - 10
+        text_y = frame_height-50
+        cv2.putText(annotated_frame, text_whist, (text_x, text_y), font, font_scale, color, thickness)
+        ##########
+        text_whist = f"{distance_whist_change}"
+        font = cv2.FONT_HERSHEY_SIMPLEX
+        font_scale = 1
+        color = (255, 250, 250)  # Dark blue in BGR
+        thickness = 2
+        text_size = cv2.getTextSize(text_whist, font, font_scale, thickness)[0]
+        text_x = frame_width - text_size[0] - 15
+        text_y = frame_height-20
+        cv2.putText(annotated_frame, text_whist, (text_x, text_y), font, font_scale, color, thickness)
+        ## Movements annotations
+        """
         text = f"Movement: {movements}"
         font = cv2.FONT_HERSHEY_SIMPLEX
         font_scale = 1
@@ -64,6 +138,7 @@ while True:
         text_x = frame_width - text_size[0] - 10
         text_y = 30
         cv2.putText(annotated_frame, text, (text_x, text_y), font, font_scale, color, thickness)
+        """
 
     # Write the frame to the output video
     out.write(annotated_frame)
